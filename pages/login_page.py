6f5139262f28e15de9c1c4d714eaf2d2ffd9eb68
Sunil Dutt <sunil.dutt28@gmail.com>
2026-07-24 19:42:28 +0100

    login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,31 +1,29 @@
 from conftest import page
 from utils.logger import logger
 from playwright.sync_api import expect
+from pages.base_page import BasePage
 
-class LoginPage:
-
+class LoginPage(BasePage):
 
     def __init__(self, page):
-        self.page = page
+        super().__init__(page)
 
-        self.username = self.page.locator("[data-test='username']")
-        self.password = self.page.locator("[data-test='password']")
-        self.login_button = self.page.locator("[data-test='login-button']")
-        self.error_message = self.page.locator("[data-test='error']")
+        self.username = "[data-test='username']"
+        self.password = "[data-test='password']"
+        self.login_button = "[data-test='login-button']"
+        self.error_message = "[data-test='error']"
 
 
     def open(self, url):
         logger.info(f"Opening URL: {url}")
         self.page.goto(url)
 
-    def login(self, username, password):
-        logger.info(f"Logging in as {username}")
-        self.username.fill(username)
-        self.password.fill(password)
-        self.login_button.click()
+    def login(self):
+        logger.info(f"Logging in as {self.username}")
+        self.username.fill(self.username)
+        self.password.fill(self.password)
+        self.click(self.login_button)
         
 
-    
-
     def verify_error_message(self, expected_message):
-        expect(self.error_message).to_have_text(expected_message)
+        expect(self.get_text(self.error_message)).to_have_text(expected_message)
