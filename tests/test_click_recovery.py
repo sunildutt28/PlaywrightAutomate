e1c3ddc9570bff8b87b7c9f627d36a1d0d942a40
Sunil Dutt <sunil.dutt28@gmail.com>
2026-08-01 17:59:17 +0100

    Testing GitHub webhook

---
+++ tests/test_click_recovery.py
@@ -0,0 +1,35 @@
+from pages.base_page import BasePage
+from ai.element_profile import ElementProfile
+import pytest
+
+@pytest.mark.smoke
+def test_click_recovery(page, base_url):
+
+    page.goto(base_url)
+
+    base_page = BasePage(page)
+
+    profile = ElementProfile(
+        locator="#user-name",
+        tag="input",
+        text="",
+        role="",
+        element_id="user-name",
+        element_type="text",
+        aria_label="",
+        placeholder="Username",
+        css_class="form_input",
+        parent_tag="div"
+    )
+
+    # Store the profile
+    base_page.profile_store.save(
+    "username",
+    profile
+)
+
+    base_page.click_with_recovery(
+    "login_page",
+    "username",
+    "#user-names"   # Wrong locator to trigger recovery
+)
