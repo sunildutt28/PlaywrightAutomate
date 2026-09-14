2c8c19bb8b66da15181a2b677f12ab348af40164
Sunil Dutt Bangalore Ramesh <125875046+sunildutt28@users.noreply.github.com>
2026-09-14 15:11:26 +0100

    Add test for login functionality

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,6 +2,7 @@ import pytest
 from pages.login_page import LoginPage
 from test_data.login_data import LOGIN_USERS
 
+#test for login
 
 @pytest.mark.parametrize(
     "username,password,expected_success,expected_message",
